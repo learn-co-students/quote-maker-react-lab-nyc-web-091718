3235170a58a696566306007191c8caa8eb40dde4
AsafDavidov <adavidov@conncoll.edu>
2018-12-09 17:10:25 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,22 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state,{...action.quote,votes:0}]
+    case "REMOVE_QUOTE":
+      return state.filter(q=>q.id!==action.quoteId)
+    case "UPVOTE_QUOTE":
+      return state.map(q=>{
+        if (q.id === action.quoteId){
+          return {...q,votes:q.votes+1}
+        }else return q
+      })
+    case "DOWNVOTE_QUOTE":
+    return state.map(q=>{
+      if (q.id === action.quoteId && q.votes>0){
+        return {...q,votes:q.votes-1}
+      }else return q
+    })
+    default:
+      return state
+  }
 }
