9f07bfd1af1a2d7fd002e3a5d241e9c68a4b4dd9
asc5025 <albert.s.chun@gmail.com>
2019-01-01 11:23:49 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
 export default (state = [], action) => {
-  return state;
+  let targetQuote;
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return state.concat(action.quote)
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      targetQuote = state.find(q => q.id === action.quoteId)
+      return [...state.filter(q => q.id !== action.quoteId), {...targetQuote, votes: targetQuote.votes + 1 }]
+    case "DOWNVOTE_QUOTE":
+      targetQuote = state.find(q => q.id === action.quoteId)
+      if (targetQuote.votes > 0 ) {
+        return [...state.filter(q => q.id !== action.quoteId), {...targetQuote, votes: targetQuote.votes - 1 }]
+      }
+      return state;
+    default:
+      return state;
+  }
 }
